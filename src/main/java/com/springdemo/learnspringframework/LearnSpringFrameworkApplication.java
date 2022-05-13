afffe3abf37532cf6b6dca76491be80b835be709
Rich Hoyt <rhoyt10@gmail.com>
2022-05-12 20:11:19 -0400

    Add spring wiring

--- src/main/java/com/springdemo/learnspringframework/LearnSpringFrameworkApplication.java
+++ src/main/java/com/springdemo/learnspringframework/LearnSpringFrameworkApplication.java
@@ -1,16 +1,18 @@
 package com.springdemo.learnspringframework;
 
 import com.springdemo.learnspringframework.game.GameRunner;
-import com.springdemo.learnspringframework.game.SuperContraGame;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
-		SuperContraGame game = new SuperContraGame();
-		GameRunner runner = new GameRunner(game);
+		ConfigurableApplicationContext context =
+				SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		//MarioGame, GameRunner
+		GameRunner runner = context.getBean(GameRunner.class);
 		runner.runGame();
 	}
-
 }
